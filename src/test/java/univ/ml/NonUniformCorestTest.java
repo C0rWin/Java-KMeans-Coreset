36d0d63a8a8365cba47a8df5c0864db9dc932496
Artem Barger <bartem@il.ibm.com>
2016-02-29 23:18:30 +0200

    Add random tests

--- src/test/java/univ/ml/NonUniformCorestTest.java
+++ src/test/java/univ/ml/NonUniformCorestTest.java
@@ -1,5 +1,7 @@
 package univ.ml;
 
+import com.carrotsearch.randomizedtesting.RandomizedTest;
+import com.carrotsearch.randomizedtesting.annotations.Repeat;
 import com.google.common.collect.Lists;
 import org.apache.commons.math3.random.RandomDataGenerator;
 import org.junit.Assert;
@@ -7,18 +9,43 @@ import org.junit.Test;
 
 import java.util.List;
 
-public class NonUniformCorestTest {
+public class NonUniformCorestTest extends RandomizedTest {
 
     @Test
+    @Repeat(iterations = 100)
     public void sampleSizeSmallerThanOverAllPointsShouldReturnAllPoints() {
-        final int k = 10;
-        final int sampleSize = 100;
-        final int dimension = 100;
-        final int datasetSize = 1_000;
+        final int k = randomIntBetween(5, 20);
+        final int sampleSize = randomIntBetween(250, 300);
+        final int dimension = randomIntBetween(10, 200);
+        final int datasetSize = randomIntBetween(50, 200);
 
         final NonUniformCoreset<WeightedDoublePoint> alg = new NonUniformCoreset<>(k, sampleSize);
         final RandomDataGenerator rnd = new RandomDataGenerator();
 
+        List<WeightedDoublePoint> points = generateRandomSet(dimension, datasetSize, rnd);
+        final List<WeightedDoublePoint> sample = alg.takeSample(points);
+
+        Assert.assertEquals(datasetSize, sample.size());
+    }
+
+    @Test
+    @Repeat(iterations = 100)
+    public void sampleSizeGreaterThanOverAllPointsShouldReturnSampleSize() {
+        final int k = randomIntBetween(5, 20);
+        final int sampleSize = randomIntBetween(50, 300);
+        final int dimension = randomIntBetween(10, 200);
+        final int datasetSize = randomIntBetween(250, 1_000);
+
+        final NonUniformCoreset<WeightedDoublePoint> alg = new NonUniformCoreset<>(k, sampleSize);
+        final RandomDataGenerator rnd = new RandomDataGenerator();
+
+        List<WeightedDoublePoint> points = generateRandomSet(dimension, datasetSize, rnd);
+        final List<WeightedDoublePoint> sample = alg.takeSample(points);
+
+        Assert.assertTrue(sampleSize>=sample.size());
+    }
+
+    private List<WeightedDoublePoint> generateRandomSet(int dimension, int datasetSize, RandomDataGenerator rnd) {
         List<WeightedDoublePoint> points = Lists.newArrayList();
 
         for (int j = 0; j < datasetSize; j++) {
@@ -28,10 +55,7 @@ public class NonUniformCorestTest {
             }
             points.add(new WeightedDoublePoint(coord, 1, ""));
         }
-
-        final List<WeightedDoublePoint> sample = alg.takeSample(points);
-
-        Assert.assertEquals(sampleSize, sample.size());
+        return points;
     }
 
 }
