8c0f7ddf458ad1628b98bae5497c009c878a0b00
Roy Levin <royl@il.ibm.com>
2016-03-24 16:45:21 +0200

    make algorithms serializable for spark

--- src/main/java/univ/ml/sparse/algorithm/SparseCoresetAlgorithm.java
+++ src/main/java/univ/ml/sparse/algorithm/SparseCoresetAlgorithm.java
@@ -2,9 +2,10 @@ package univ.ml.sparse.algorithm;
 
 import univ.ml.sparse.SparseWeightableVector;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface SparseCoresetAlgorithm {
+public interface SparseCoresetAlgorithm extends Serializable {
 
     List<SparseWeightableVector> takeSample(final List<SparseWeightableVector>  pointset);
 
