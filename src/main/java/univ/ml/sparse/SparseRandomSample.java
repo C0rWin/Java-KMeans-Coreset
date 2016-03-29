5370680838dcff1c149ca042e54f839d1892c23c
Roy Levin <royl@il.ibm.com>
2016-03-29 17:43:41 +0300

    fix prob=0

--- src/main/java/univ/ml/sparse/SparseRandomSample.java
+++ src/main/java/univ/ml/sparse/SparseRandomSample.java
@@ -28,12 +28,14 @@ public class SparseRandomSample {
         final Random rnd = new Random();
 
         for (int j = 0; j < t; j++) {
-            double cdf = total * rnd.nextDouble();
+        	double pr = rnd.nextDouble();
+            double cdf = total * pr;
+            
             int i  = 0;
             for (double sum = 0; sum < cdf; i++) {
                 sum += dataset.get(i).getProbability();
             }
-            result.add(dataset.get(i-1));
+            result.add(dataset.get(Math.max(0, i-1)));
         }
 
         return result;
